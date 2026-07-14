706ced575b65fabfc2687474498cd4c0d24e379f
FarrukhKhan01 <thefarrukhkhan@hotmail.com>
2026-07-14 05:02:08 +0100

    Initial Angular Project Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { Signup } from './features/auth/pages/signup/signup';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup },
+    { path: 'dasboard', component: Dashboard },
+
+];
+
